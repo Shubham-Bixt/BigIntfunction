d5a404a37e4171eada30f5937757d1c6451559f6
Shubham Bisht <148553546+Shubham-Bixt@users.noreply.github.com>
2023-10-20 23:10:50 +0530

    Update bigint_operations.c

--- bigint_operations.c
+++ bigint_operations.c
@@ -2,44 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void initBigInt(BigInt* num) {
+void initBigInt(BigInt* num)
+{
     num->head = NULL;
 }
 
-void insertDigit(BigInt* num, int digit) {
+void insertDigit(BigInt* num, int digit) 
+{
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = digit;
     newNode->next = num->head;
     num->head = newNode;
 }
 
-void addBigInts(BigInt* num1, BigInt* num2, BigInt* result) {
-    int carry = 0;
-    Node* ptr1 = num1->head;
-    Node* ptr2 = num2->head;
 
-    while (ptr1 != NULL || ptr2 != NULL || carry != 0) {
-        int sum = carry;
-        if (ptr1 != NULL) {
-            sum += ptr1->data;
-            ptr1 = ptr1->next;
-        }
-        if (ptr2 != NULL) {
-            sum += ptr2->data;
-            ptr2 = ptr2->next;
-        }
-        carry = sum / 10;
-        sum = sum % 10;
-        insertDigit(result, sum);
-    }
-}
 
-void subtractBigInts(BigInt* num1, BigInt* num2, BigInt* result) {
+void subtractBigInts(BigInt* num1, BigInt* num2, BigInt* result) 
+{
     int borrow = 0;
     Node* ptr1 = num1->head;
     Node* ptr2 = num2->head;
 
-    while (ptr1 != NULL || ptr2 != NULL || borrow != 0) {
+    while (ptr1 != NULL || ptr2 != NULL || borrow != 0)
+        {
         int diff = borrow;
         if (ptr1 != NULL) {
             diff += ptr1->data;
@@ -59,21 +44,60 @@ void subtractBigInts(BigInt* num1, BigInt* num2, BigInt* result) {
     }
 }
 
-void displayReversedBigInt(BigInt* num) {
+void addBigInts(BigInt* num1, BigInt* num2, BigInt* result)
+{
+    int carry = 0;
+    Node* ptr1 = num1->head;
+    
+    Node* ptr2 = num2->head;
+    
+
+    while (ptr1 != NULL || ptr2 != NULL || carry != 0) {
+        int sum = carry;
+        
+        if (ptr1 != NULL) 
+        {
+            
+            sum += ptr1->data;
+            
+            ptr1 = ptr1->next;
+        }
+        if (ptr2 != NULL)
+        {
+            sum += ptr2->data;
+            ptr2 = ptr2->next;
+        }
+        carry = sum / 10;
+        
+        sum = sum % 10;
+        
+    
+        insertDigit(result, sum);
+    }
+}
+
+void displayReversedBigInt(BigInt* num) 
+{
     Node* current = num->head;
     while (current != NULL) {
+        
         printf("%d", current->data);
         current = current->next;
+        
     }
     printf("\n");
 }
 
-void clearBigInt(BigInt* num) {
+void clearBigInt(BigInt* num) 
+{
     Node* current = num->head;
+    
     while (current != NULL) {
+        
         Node* temp = current;
         current = current->next;
         free(temp);
+        
     }
     num->head = NULL;
 }
