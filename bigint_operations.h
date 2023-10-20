d7bccea193f0ea751d50ee8790554e0822abc9c8
Shubham Bisht <148553546+Shubham-Bixt@users.noreply.github.com>
2023-10-20 23:11:56 +0530

    Update bigint_operations.h

--- bigint_operations.h
+++ bigint_operations.h
@@ -1,20 +1,29 @@
 #ifndef BIGINT_OPERATIONS_H
+
 #define BIGINT_OPERATIONS_H
 
-typedef struct Node {
+typedef struct Node 
+{
     int data;
     struct Node* next;
 } Node;
 
-typedef struct BigInt {
+typedef struct BigInt
+{
     Node* head;
 } BigInt;
 
-void initBigInt(BigInt* num);
+
 void insertDigit(BigInt* num, int digit);
+
+void initBigInt(BigInt* num);
+
+void subtractBigInts(BigInt* num1, BigInt* num2, BigInt* result); 
+
 void addBigInts(BigInt* num1, BigInt* num2, BigInt* result);
-void subtractBigInts(BigInt* num1, BigInt* num2, BigInt* result); // Added subtraction function
-void displayReversedBigInt(BigInt* num);
+
 void clearBigInt(BigInt* num);
+
+void displayReversedBigInt(BigInt* num);
 
 #endif
